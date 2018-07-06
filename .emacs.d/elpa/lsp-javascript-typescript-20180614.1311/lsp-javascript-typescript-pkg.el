e950bb61e91e1fe32dc53d74cdbc0061e00f6a57
Charlie Stanton <charlie@shtanton.com>
2018-07-06 07:55:32 +0100

    Adds emacs config

---
+++ .emacs.d/elpa/lsp-javascript-typescript-20180614.1311/lsp-javascript-typescript-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-javascript-typescript" "20180614.1311" "Javascript/Typescript support for lsp-mode" '((lsp-mode "3.0") (typescript-mode "0.1") (emacs "25.1")))
