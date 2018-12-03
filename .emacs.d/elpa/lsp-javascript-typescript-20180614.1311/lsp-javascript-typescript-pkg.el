947c81fc851edbc14c75a79876b5d0b9d146f519
Charlie Stanton <charlie@shtanton.com>
2018-12-03 14:52:12 +0000

    bye emacs

--- .emacs.d/elpa/lsp-javascript-typescript-20180614.1311/lsp-javascript-typescript-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-javascript-typescript" "20180614.1311" "Javascript/Typescript support for lsp-mode" '((lsp-mode "3.0") (typescript-mode "0.1") (emacs "25.1")))
--- .emacs.d/elpa/nord-theme-20180102.1001/nord-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "nord-theme" "20180102.1001" "An arctic, north-bluish clean and elegant theme" '((emacs "24")))
